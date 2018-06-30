ab3e821e27ab4808d350e0114ae81f185ff5be46
mloweedgar <mloweedgar0@gmail.com>
2018-06-30 03:17:43 +0300

    Currency converter without offiline capabilities

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,6 +1,46 @@
 'use strict';
 
-var test = function test() {
-  return console.log('testing es6 feature');
+var getCurrency = function getCurrency() {
+    return fetch('https://free.currencyconverterapi.com/api/v5/currencies').then(function (res) {
+        return res.json();
+    }).then(function (myJson) {
+        return myJson.results;
+    }).then(function (data) {
+        initilizeCurrencyList(data, 'from_currency', 'TZS');
+        initilizeCurrencyList(data, 'to_currency', 'USD');
+    });
 };
-test();
+
+var initilizeCurrencyList = function initilizeCurrencyList(currency, selectId, selectedCurrency) {
+    var select = document.getElementById(selectId);
+    for (var curr in currency) {
+        var option = document.createElement('option');
+        option.value = curr;
+        option.text = curr;
+        if (curr == selectedCurrency) option.selected = "selected";
+        select.appendChild(option);
+    }
+};
+
+var convertCurrency = function convertCurrency(rate, rateString) {
+    var val = rate[rateString].val;
+
+    var inputMoney = document.getElementById('input_money').value;
+    var outPutMoney = inputMoney * val;
+    document.getElementById('output_money').value = outPutMoney;
+    console.log(outPutMoney);
+};
+
+var onConvert = function onConvert() {
+    var from = document.getElementById('from_currency').value;
+    var to = document.getElementById('to_currency').value;
+    var rateString = from + '_' + to;
+    var url = 'https://free.currencyconverterapi.com/api/v5/convert?q=' + rateString + '&compact=y';
+    fetch(url).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        return convertCurrency(data, rateString);
+    });
+};
+
+getCurrency();
--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
-let test = () => console.log('testing es6 feature');
-test();
+const getCurrency = () =>fetch('https://free.currencyconverterapi.com/api/v5/currencies')
+.then(res => res.json())
+.then(myJson => myJson.results)
+.then(data => {
+    initilizeCurrencyList(data, 'from_currency', 'TZS');
+    initilizeCurrencyList(data, 'to_currency', 'USD');
+});
+
+const initilizeCurrencyList = (currency, selectId, selectedCurrency) => {
+    let select = document.getElementById(selectId);
+    for (let curr in currency) {
+        let option = document.createElement('option');
+        option.value = curr;
+        option.text = curr;
+        if(curr == selectedCurrency) option.selected = "selected";
+        select.appendChild(option);
+    }
+};
+
+const convertCurrency = (rate, rateString) => {
+    let {val} = rate[rateString];
+    let inputMoney = document.getElementById('input_money').value;
+    let outPutMoney = inputMoney * val;
+    document.getElementById('output_money').value = outPutMoney;
+    console.log(outPutMoney);
+
+};
+
+const onConvert = () =>{
+    let from = document.getElementById('from_currency').value;
+    let to = document.getElementById('to_currency').value;
+    let rateString = `${from}_${to}`;
+    let url = `https://free.currencyconverterapi.com/api/v5/convert?q=${rateString}&compact=y`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => convertCurrency(data, rateString));   
+};
+
+
+getCurrency();
